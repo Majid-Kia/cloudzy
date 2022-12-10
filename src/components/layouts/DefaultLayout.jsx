1ca109d5528d1d96c4c457181b4952f797a791a9
majid <majidkia.dev@gmail.com>
2022-12-10 12:58:26 +0330

    add a log for test

--- src/components/layouts/DefaultLayout.jsx
+++ src/components/layouts/DefaultLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../navbar/Navbar";
 
 export default function Layout({ children }) {
+  console.log("test");
   return (
     <div className="container">
       <Navbar />
